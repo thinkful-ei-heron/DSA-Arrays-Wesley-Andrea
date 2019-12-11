3b5286c6989cddb31e766a3c5cca9db5b1ad439a
Andrea Bender <andreab.1726@gmail.com>
2019-12-11 10:42:08 -0700

    completed 5

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-console */
+/* eslint-disable indent */
+/* eslint-disable strict */
 const drillArray = require('./array');
 
 
-function main(){
+function main() {
     drillArray.SIZE_RATIO = 3;
 
     let arr = new drillArray();
@@ -12,6 +15,23 @@ function main(){
     arr.push(19);
     arr.push(45);
     arr.push(10);
+
+    arr.pop();
+    arr.pop();
+    arr.pop();
+
+    // console.log(arr.get(0));
+    for (let i = 0; i < arr.length; i++) {
+        arr.pop();
+    }
+    arr.push('tauhida');
+    console.log(arr.get(1));
+    //NaN being returned, undefined?
+
+    //resize
+    //if arr.length >= capacity resize will increase capacity 
+    // based on the ratio you provide 
+
     console.log(arr);
 }
 
@@ -19,4 +39,15 @@ main();
 // drillArray { length: 1, _capacity: 1, ptr: 0 }
 // with added pushes drillArray { length: 6, _capacity: 6, ptr: 15 }
 // The ptr is 3 times the original size of the array due to the ._resize method.
+//drillArray { length: 3, _capacity: 6, ptr: 15 }
+//.pop decreses the length, but capacity stays the sam
+// values not removed because memory has not been overwritten
 
+//URLify a string
+//replace spaces with %20
+
+function urlify(str) {
+    let res = str.replace(' ', '%20')
+    return res;
+}
+console.log(urlify('test code'));
